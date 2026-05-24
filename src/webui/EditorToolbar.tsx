4976108261cbb0b7bc1e341ff0454f19f43ef5f5
LesterCamp234 <lestercamp377@gmail.com>
2026-05-24 11:21:31 +0200

    added light icon

--- src/webui/EditorToolbar.tsx
+++ src/webui/EditorToolbar.tsx
@@ -1,9 +1,10 @@
 import { Component, Show } from "solid-js";
 import { prefixStr, setIntegratedHelp, testsuiteName } from "./App";
-import { doChangeTheme } from "./Theme";
+import {currentTheme, doChangeTheme} from "./Theme";
 import { continueExecution, nextStep, quitDebug, reverseStep, run, runTestSuite, singleStep, startDebug, state } from "./EmulatorStore";
+import {githubLight} from "./GithubTheme";
 
-// to rebuild font.woff2, download https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0&icon_names=arrow_forward,close,dark_mode,folder_open,help,play_circle,resume,save,step_into,step_over,stop,undo
+// to rebuild font.woff2, download https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0&icon_names=arrow_forward,close,dark_mode,folder_open,help,play_circle,resume,save,step_into,step_over,stop,sunny,undo
 
 export const EditorToolbar: Component<{ textGetter: () => string, setText: (s: string) => void }> = (props) => {
     return (
@@ -25,7 +26,7 @@ export const EditorToolbar: Component<{ textGetter: () => string, setText: (s: s
 
                     <ToolbarBtn
                         class="theme-bg"
-                        icon="dark_mode"
+                        icon={currentTheme() == githubLight ? "sunny" : "dark_mode"}
                         title="Change theme"
                         onClick={doChangeTheme}
                     />
